51883f92e02f4ebad68d1bcd5ff4a9c4c3d0e1e6
mattmeyerink <meyerink@umich.edu>
2022-04-09 11:03:42 -0700

    set up basic dvy app info page structure

--- src/components/DVY/DVYAppInfo.js
+++ src/components/DVY/DVYAppInfo.js
@@ -1,11 +1,23 @@
-import UnderConstruction from "../UnderConstruction";
+import DVYLogo from "../../images/DVY.png";
 
 function DVYAppInfo() {
     return (
         <div className="appInfoContainer">
             <h1>DVY App Info</h1>
-         
-            <UnderConstruction />
+
+            <div className="appInfoContainer2">
+                <div>
+                    <img src={DVYLogo} alt="DVY Logo" className="appLogoContainer" />
+                </div>
+
+                <div className="appInfoText">
+                    <h4>Description</h4>
+                    <p>This is where all of that random description text will go that will get people excited about the app and shit</p>
+
+                    <h4>Built With</h4>
+                    <p>This is where I can be a nerd and geek out about the cool shit that I have used to make the app cool</p>
+                </div>
+            </div>
         </div>
     );
 }
